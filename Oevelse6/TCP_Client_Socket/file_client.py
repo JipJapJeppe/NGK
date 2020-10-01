f8c8cef2b5c4c5fd5c017de43b7450dc27a4b9a9
AndreasSteffensen <55832629+AndreasSteffensen@users.noreply.github.com>
2020-10-01 11:11:06 +0200

    Client
    cool Client

--- Oevelse6/TCP_Client_Socket/file_client.py
+++ Oevelse6/TCP_Client_Socket/file_client.py
@@ -9,18 +9,33 @@ serverName = "10.0.0.1"
 
 def main(argv):
 	# TO DO Your Code
-	clientSocket = socket(AF_INET,SOCK_STREAM)
+	s = socket(AF_INET,SOCK_STREAM)
 	print("The client is connecting...")
 	clientSocket.connect((serverName, PORT))
 	print ("Client Connected")
-	# WriteTextTCP()
-
-	#readTextTCP or getFileSizeTCP() 
+    filename = input("Filename? -> ")
 	
-	# 
+    if (filename != "q"):
+        s.send(filename.encode())
+        data = s.recv(1024)
+        if (data[:6] == "EXISTS"):
+            filesize = long(data[6:])
+            message = input("File Exists, " + str(fielsize) + "Bytes, download? (Y/N)? -> ");
+            if (message == "Y"):
+                s.send("OK")
+                f = open('new_'+filename,'wb')
+                data = s.recv(1024)
+                totalRecv = len(data)
+                f.write(data)
+                while(totalRecv < filesize):
+                    data = s.recv(1024)
+                    totalRecv += len(data)
+                    f.write(data)
+                    print("{0:.2f}".format((totalRecv/float(filesize))*100 + "%Done"));
+                	print("Download Complete!")
 
-def receiveFile(fileName,  conn):
-	# TO DO Your Code
+        else:
+            print("File does not exist!");	
 
 
 clientSocket.close()
