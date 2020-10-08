c2469b95250b674b286a93e232970c4e6b8f53e6
AndreasSteffensen <55832629+AndreasSteffensen@users.noreply.github.com>
2020-10-08 10:00:45 +0200

    Working client v.1
    Forhåbenligt virker det.

--- Oevelse6/TCP_Client_Socket/file_client.py
+++ Oevelse6/TCP_Client_Socket/file_client.py
@@ -9,32 +9,32 @@ serverName = "10.0.0.1"
 
 def main(argv):
     s = socket(AF_INET,SOCK_STREAM)
-    print("The client is connecting...")
+    #print("The client is connecting...")
     s.connect((serverName, PORT))
-    print ("Client Connected")
+    #print ("Client Connected")
+    
     filename = input("Filename? -> ")
-    
     if filename != 'q':
-        #s.send(filename.encode())
-        #data = s.recv(1024)
-        #if (data[:6] == "EXISTS"):
-            #filesize = long(data[6:])
-            #message = input("File Exists, " + str(filesize) + "Bytes, download? (Y/N)? -> ")
-            #if (message == "Y"):
-            #    s.send("OK")
-            #    f = open('new_'+filename,'wb')
-            #    data = s.recv(1024)
-            #    totalRecv = len(data)
-            #    f.write(data)
-                #while(totalRecv < filesize):
-                #   data = s.recv(1024)
-                #    totalRecv += len(data)
-                #    f.write(data)
-                #    print("{0:.2f}".format((totalRecv/float(filesize))*100 + "%Done"))
-                #    print("Download Complete!")
-        #else:
-            #print("File does not exist!")
-    #clientSocket.close()
-    
-    if __name__ == "__main__":
-        main(sys.argv[1:])
+        s.send(filename.encode())
+        data = s.recv(1024)
+        if (data[:6] == "EXISTS"):
+            filesize = long(data[6:])
+            message = input("File Exists, " + str(filesize) + "Bytes, download? (Y/N)? -> ")
+            if (message == "Y"):
+                s.send("OK")
+                f = open('new_'+filename,'wb')
+                data = s.recv(1024)
+                totalRecv = len(data)
+                f.write(data)
+                while(totalRecv < filesize):
+                    data = s.recv(1024)
+                    totalRecv += len(data)
+                    f.write(data)
+                    print("{0:.2f}".format((totalRecv/float(filesize))*100 + "%Done"))
+                print("Download Complete!")
+            else:
+                print("File does not exist!")
+        s.close()
+        
+if __name__ == "__main__":
+   main(sys.argv[1:])
