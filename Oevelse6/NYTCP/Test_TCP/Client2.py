4646e717ae753e07fececabd703e312ed247d303
AndreasSteffensen <Steffensen12@hotmail.com>
2020-10-15 12:12:41 +0200

    Test mappe med TCP text

--- Oevelse6/NYTCP/Test_TCP/Client2.py
+++ Oevelse6/NYTCP/Test_TCP/Client2.py
@@ -1,61 +1,27 @@
-import sys
-from socket import socket, AF_INET, SOCK_STREAM
-from lib import Lib
+import socket
 
-PORT = 9000
-serverName = "10.0.0.1"
+SERVER = '127.0.1.1'
+PORT = 5050
 BUFSIZE = 1000
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+PORT = 5050
+BUFSIZE = 1000
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-def main(argv):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    print ("The client is trying to connect")
-    clientSocket.connect((serverName, PORT))
-    print("The client is connected")
-    messagetoserver = input("Input command: ")
-    #clientSocket.sendto(messagetoserver.encode(),(socket))
-    receiveFile(messagetoserver, clientSocket)
-    #Lib.writeTextTCP(, serverName)
-    
-    #text = Lib.readTextTCP(serverName)
-    
-    #messagefromserver = bytesAddresPair[0]
-    #print(messagefromserver.decode())
-    clientSocket.close()
-    
-    
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ADDR))
 
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (BUFSIZE - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(BUFSIZE).decode(FORMAT))
 
-    #img.saved.save()
-def receiveFile(fileName,  conn):
-	# TO DO Your Code
-    print("Inside receiveFile")
-    Lib.writeTextTCP(fileName, conn)
-   
-    print("After conn.sendto")
-    #sendto(data: bytes, address: _Addresss)
-    #bytesAddresPair = conn.recvfrom(2048)
-    #number of bytes to be read from the TCP socket
-    sizeOfFile = int(Lib.readTextTCP(conn))
-    print(sizeOfFile)
-    print("after sizeOfFile")
-    if sizeOfFile == 0 :
-        print("Der findes ikke nogen fil")
-        return
-    fileNameExtracted = Lib.extractFilename(fileName)
-    fd = open(fileNameExtracted, 'wb')
-   
-    while sizeOfFile >= BUFSIZE:
-        print("Inside receiveFile Inside while loop")
-        var = Lib.readImageTCP(conn)
-        fd.write(var)
-        
-        sizeOfFile-=BUFSIZE
-        print("After fileLEngth -=")
-    print("fd.close") 
-    var = Lib.readImageTCP(conn)
-    fd.write(var)
-    fd.close()
-      
-
-if __name__ == "__main__":
-   main(sys.argv[1:])
+send("Hello World!")
+send("Hello Me!")
+send("Hello You!")
+send(DISCONNECT_MESSAGE)
